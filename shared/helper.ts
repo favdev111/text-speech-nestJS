e7daf1f0e0b3edb894a8179aa2ba0c2321fc16de
favdev <favdev@gmail.com>
2021-08-12 06:43:31 -0700

    finish funciton for uploading docx file

--- shared/helper.ts
+++ shared/helper.ts
@@ -1,14 +1,16 @@
 export class Helper {
   static customFileName(req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    let fileExtension = '';
-    if (file.mimetype.indexOf('jpeg') > -1) {
-      fileExtension = 'jpg';
-    } else if (file.mimetype.indexOf('png') > -1) {
-      fileExtension = 'png';
-    }
-    const originalName = file.originalname.split('.')[0];
-    cb(null, originalName + '-' + uniqueSuffix + '.' + fileExtension);
+    // let fileExtension = '';
+    // if (file.mimetype.indexOf('jpeg') > -1) {
+    //   fileExtension = 'jpg';
+    // } else if (file.mimetype.indexOf('png') > -1) {
+    //   fileExtension = 'png';
+    // }
+    console.log(file);
+    // const originalName = file.originalname.split('.')[0];
+    // cb(null, originalName + '-' + uniqueSuffix + '.' + file.mimetype);
+    cb(null, file.originalname);
   }
 
   static destinationPath(req, file, cb) {
